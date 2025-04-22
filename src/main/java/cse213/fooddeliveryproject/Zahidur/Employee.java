83b3bbf27dc708c35b464ac0921b306a705d4bbc
zahidur <1931407@iub.edu.bd>
2025-04-22 13:48:21 +0600

    test the employee data by own account

---
+++ src/main/java/cse213/fooddeliveryproject/Zahidur/Employee.java
@@ -0,0 +1,12 @@
+package cse213.fooddeliveryproject.Zahidur;
+
+import java.time.LocalDate;
+
+public class Employee {
+    public int employeeId;
+    public String name,position,email,phone;
+    public  double salary;
+    public LocalDate hireDate;
+    public double performanceRating;
+    private String payrollDetails;
+}
